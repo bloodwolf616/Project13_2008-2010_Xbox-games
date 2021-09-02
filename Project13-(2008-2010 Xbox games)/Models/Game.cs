ea34c8fc598990ed47ab055e84fd496d02521326
Chris Cooper <bloodwolf444@gmail.com>
2021-09-01 22:38:20 -0700

    Added my "Game" Data Model.

---
+++ Project13-(2008-2010 Xbox games)/Models/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project13__2008_2010_Xbox_games_.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string genre { get; set; }
+        public DateTime DateOfRelease { get; set; }
+        public string Description { get; set; }
+        public string Developer { get; set; }
+        
+
+        //one to many relationship
+        public string Series { get; set; }
+     
+
+
+
+    }
+}
