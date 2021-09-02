2458b81144368c0dba9af51ad9a109079f13adca
Chris Cooper <bloodwolf444@gmail.com>
2021-09-01 22:51:20 -0700

    Added the "Game"  Model to the controller.

--- Project13-(2008-2010 Xbox games)/Controllers/GameController.cs
+++ Project13-(2008-2010 Xbox games)/Controllers/GameController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Project13__2008_2010_Xbox_games_.Models;
 
 namespace Project13__2008_2010_Xbox_games_.Controllers
 {
@@ -11,11 +12,17 @@ namespace Project13__2008_2010_Xbox_games_.Controllers
         // GET: Game
         public ActionResult Index()
         {
+            Game halo3 = new Game()
+            {
+                Title = "Halo 3",
+                Genre = "First Person Shooter",
+                DateOfRelease = new DateTime(2007, 9, 27),
+                Description = "Halo 3's story centers on the interstellar war between twenty-sixth century humanity, a collection of alien races known as the Covenant, and the alien parasite Flood. The player assumes the role of the Master Chief, a cybernetically enhanced supersoldier, as he battles the Covenant and the Flood.",
+                Developer = "Bungie",
+                Series = "Halo",
 
-            ViewBag.Title = "2008-2010 Xbox Games";
-            ViewBag.Name = "Chris";
-            ViewBag.GroceryList = new string[] { "Apples", "Oranges", "Bananas" };
-            return View();
+            };
+            return View(halo3);
         }
     }
 }
--- Project13-(2008-2010 Xbox games)/Models/Game.cs
+++ Project13-(2008-2010 Xbox games)/Models/Game.cs
@@ -8,8 +8,7 @@ namespace Project13__2008_2010_Xbox_games_.Models
     public class Game
     {
         public int Id { get; set; }
-        public string title { get; set; }
-        public string genre { get; set; }
+        public string Title { get; set; }
         public DateTime DateOfRelease { get; set; }
         public string Description { get; set; }
         public string Developer { get; set; }
@@ -17,7 +16,9 @@ namespace Project13__2008_2010_Xbox_games_.Models
 
         //one to many relationship
         public string Series { get; set; }
-     
+
+        public string Genre { get; set; }
+
 
 
 
