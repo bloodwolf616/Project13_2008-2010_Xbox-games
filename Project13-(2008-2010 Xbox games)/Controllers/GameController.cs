f5476ca6af981a34c5c6f3810c1ee388632ca37d
Chris Cooper <bloodwolf444@gmail.com>
2021-09-01 20:36:18 -0700

    Added a basic controller and Index.

---
+++ Project13-(2008-2010 Xbox games)/Controllers/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project13__2008_2010_Xbox_games_.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
